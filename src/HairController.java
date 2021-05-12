1ba864c6af00319f70c9caa16a1af73646196ff0
Osmon <osmon.ahh@outlook.com>
2021-05-12 14:11:29 +0200

    small improvements in HairController

--- src/HairController.java
+++ src/HairController.java
@@ -3,31 +3,32 @@ import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.util.Random;
 
-public class HairController extends JApplet {
+public class HairController extends JApplet
+{
     public static final int WIDTH = 800;
     public static final int HEIGHT = 650;
     
-    public static void main(String[] args) {
+    public static void main( String[] args )
+    {
         JFrame frame = new JFrame();
-        frame.setTitle("Hair Simulation");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle( "Hair Simulation" );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         JApplet applet = new HairController();
         applet.init();
-        frame.getContentPane().add(applet);
+        frame.getContentPane().add( applet );
         frame.pack();
-        frame.setVisible(true);
+        frame.setVisible( true );
     }
     
-    public void init() {
+    public void init()
+    {
         JPanel panel = new HairPanel();
-        getContentPane().add(panel);
+        getContentPane().add( panel );
     }
     
-    class HairPanel extends JPanel implements Runnable {
+    class HairPanel extends JPanel implements Runnable
+    {
         // initial settings
-        int anchorX = 209;      // the anchor/initial position x
-        int anchorY = 181;      // the anchor/initial position y
-        
         double gravity = 9.8;   // the gravitational force
         double mass = 30.0;     // the mass of the spring
         double timeStep = 0.02; // pixels to skip
@@ -35,69 +36,68 @@ public class HairController extends JApplet {
         int k = 5;              // the spring constant - stiffness
         
         Hair[] hairs;
-
+        
         // head settings
         int headX = 200;
-        int headY = 100;
+        int headY = 10;
         int headWidth = 400;
         int headHeight = 400;
         
-        public HairPanel() {
-            setPreferredSize(new Dimension(HairController.WIDTH, HairController.HEIGHT));
-            setBackground(Color.white);
+        public HairPanel()
+        {
+            setPreferredSize( new Dimension( HairController.WIDTH, HairController.HEIGHT ) );
+            setBackground( Color.white );
             
-            Thread thread = new Thread(this);
+            Thread thread = new Thread( this );
             thread.start();
-    
-            // initializing hair objects with random anchors
-            hairs = new Hair[200];
-            for(int i = 0; i < hairs.length; i++) {
-                double[] randomCoordinates = randomCoordinates();
-
-                hairs[i] = new Hair(gravity, mass, timeStep, damping, k,
-                        randomCoordinates[0], randomCoordinates[1], 7);
-            }
-        }
-
-        private double[] randomCoordinates() {
-            double theta = getRandomNumber(0, Math.PI * 2);
-            double r = getRandomNumber(0d, 1d);
-
-            double x = headWidth * Math.sqrt(r) * Math.cos(theta)/2;
-            double y = headHeight * Math.sqrt(r) * Math.sin(theta)/2;
-
-            return new double[]{headX + (headWidth / 2.0) * 0.95 + x, headY + (headHeight / 2.0) * 0.95 + y};
-        }
-        
-        public void paintComponent(Graphics g) {
-            Graphics2D g2D = (Graphics2D) g;
-            super.paintComponent(g2D);
-            RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-            g2D.setRenderingHints(rh);
-            g.setColor(Color.BLACK);
             
-            Ellipse2D head = new Ellipse2D.Double(headX, headY, headWidth, headHeight);
-            g2D.draw(head);
-
-            for (Hair hair : hairs) {
-                hair.drawHair(g2D);
+            // initializing hair objects with random anchors
+            hairs = new Hair[100];
+            for( int i = 0; i < hairs.length; i++ )
+            {
+                double[] randomCoordinates = randomCoordinates();
+                hairs[i] = new Hair( gravity, mass, timeStep, damping, k, randomCoordinates[0], randomCoordinates[1] - 10, 7 );
             }
         }
         
-        public void run() {
-            while(true) {
+        private double[] randomCoordinates()
+        {
+            double theta = getRandomNumber( 0, Math.PI * 2 );
+            double r = getRandomNumber( 0d, 1d );
+            
+            double x = headWidth * Math.sqrt( r ) * Math.cos( theta ) / 2;
+            double y = headHeight * Math.sqrt( r ) * Math.sin( theta ) / 2;
+            
+            return new double[]{ headX + ( headWidth / 2.0 ) * 0.95 + x, headY + ( headHeight / 2.0 ) * 0.95 + y };
+        }
+        
+        public void paintComponent( Graphics g )
+        {
+            Graphics2D g2D = ( Graphics2D ) g;
+            super.paintComponent( g2D );
+            RenderingHints rh = new RenderingHints( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
+            rh.put( RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY );
+            g2D.setRenderingHints( rh );
+            g.setColor( Color.BLACK );
+            
+            Ellipse2D head = new Ellipse2D.Double( headX, headY, headWidth, headHeight );
+            g2D.draw( head );
+    
+            for( Hair hair : hairs ) hair.drawHair( g2D );
+        }
+        
+        public void run()
+        {
+            while( true )
+            {
                 repaint();
                 //try { Thread.sleep( 1 ); } catch( InterruptedException ex ) {}
             }
         }
-
-        public int getRandomNumber(int min, int max) {
-            return (int) ((Math.random() * (max - min)) + min);
-        }
-
-        public double getRandomNumber(double min, double max) {
-            return min + (max - min) * new Random().nextDouble();
+        
+        public double getRandomNumber( double min, double max )
+        {
+            return min + ( max - min ) * new Random().nextDouble();
         }
     }
 }
